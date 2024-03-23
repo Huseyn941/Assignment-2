143737b1dc5f31323e90e35e8bddea82eb3d6a37
Huseyn941 <160209106+Huseyn941@users.noreply.github.com>
2024-03-23 22:00:23 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -4,6 +4,13 @@ def main():
   '''
   Kodunuzu buraya yazin.
   '''
+  herf = input("Enter a letter of the alphabet: ")
+if herf=='a' or herf=='e' or herf=='i' or herf=='o' or herf=='u':
+    print("Entered alphabet is a vowel!")
+elif herf=='y':
+    print("Sometimes it is a vowel, and sometimes it is a consonant!")
+else:
+    print("Entered alphabet is a consonant!")
   pass
 
 if __name__ == "__main__":
